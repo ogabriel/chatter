c7a38ad090f12a881560ad8235138fd1ecec1075
ogabriel <gabrieloliver8991@gmail.com>
2020-08-02 17:04:43 -0300

    remove show and index for users

--- lib/chatter_web/controllers/user_controller.ex
+++ lib/chatter_web/controllers/user_controller.ex
@@ -4,11 +4,6 @@ defmodule ChatterWeb.UserController do
   alias Chatter.Accounts
   alias Chatter.Accounts.User
 
-  def index(conn, _params) do
-    users = Accounts.list_users()
-    render(conn, "index.html", users: users)
-  end
-
   def new(conn, _params) do
     changeset = Accounts.change_user(%User{})
     render(conn, "new.html", changeset: changeset)
@@ -24,11 +19,6 @@ defmodule ChatterWeb.UserController do
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
-  end
-
-  def show(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    render(conn, "show.html", user: user)
   end
 
   def edit(conn, %{"id" => id}) do
