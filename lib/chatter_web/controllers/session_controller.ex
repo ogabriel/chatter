90448622b930856ac44299232fa7a2175db7a925
ogabriel <gabrieloliver8991@gmail.com>
2020-08-02 23:56:54 -0300

    add methods for new/create and delete on session

---
+++ lib/chatter_web/controllers/session_controller.ex
@@ -0,0 +1,33 @@
+defmodule ChatterWeb.SessionController do
+  use ChatterWeb, :controller
+
+  alias Chatter.Accounts
+
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"session" => session_params}) do
+    user = Accounts.get_user_by_email(session_params["email"])
+
+    case Bcrypt.check_pass(user, session_params["password"]) do
+      {:ok, user} ->
+        conn
+        |> put_session(:current_user_id, user.id)
+        |> put_flash(:info, "Signed in successfully.")
+        |> redirect(to: Routes.user_path(conn, :show, user))
+
+      {:error, _} ->
+        conn
+        |> put_flash(:error, "Wrong email or password")
+        |> render("new.html")
+    end
+  end
+
+  def delete(conn, _) do
+    conn
+    |> delete_session(:current_user_id)
+    |> put_flash(:info, "Signed out successfully.")
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
+end
