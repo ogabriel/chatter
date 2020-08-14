d2f74c11fff836c7407cdd9e45207c9e3f30f981
ogabriel <gabrieloliver8991@gmail.com>
2020-08-14 09:41:58 -0300

    add basic actions to password_reset_controller

--- lib/chatter_web/controllers/password_reset_controller.ex
+++ lib/chatter_web/controllers/password_reset_controller.ex
@@ -1,9 +1,23 @@
 defmodule ChatterWeb.PasswordResetController do
   use ChatterWeb, :controller
 
+  alias Chatter.Accounts
+
   def new(conn, _params) do
+    render(conn, "new.html")
   end
 
-  def create(conn, _params) do
+  def create(conn, %{"password_reset" => password_reset_params}) do
+    case Accounts.get_user_by_email(password_reset_params["email"]) do
+      nil ->
+        conn
+        |> put_flash(:error, "No email exists!")
+        |> redirect(to: Routes.password_reset_path(conn, :new))
+
+      user ->
+        conn
+        |> put_flash(:info, "Password reset e-mail was sent")
+        |> redirect(to: Routes.session_path(conn, :new))
+    end
   end
 end
