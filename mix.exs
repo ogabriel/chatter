7ea685ea774369e2a3fadd1af1c186523bb0b21f
ogabriel <gabrieloliver8991@gmail.com>
2020-08-14 09:43:24 -0300

    add bamboo dep

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule Chatter.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:comeonin, "~> 5.3"},
-      {:bcrypt_elixir, "~> 2.0"}
+      {:bcrypt_elixir, "~> 2.0"},
+      {:bamboo, "~> 1.5"}
     ]
   end
 
