2cb6bfa60e5cb8c06f2c7da77dcce5249fe80d77
ogabriel <gabrieloliver8991@gmail.com>
2020-08-16 16:15:21 -0300

    add password_reset email

--- lib/chatter/email.ex
+++ lib/chatter/email.ex
@@ -4,8 +4,14 @@ defmodule Chatter.Email do
   def base_email do
     new_email
     |> from("email@chatter.com")
+    |> put_html_layout({TeacherWeb.LayoutView, "email.html"})
   end
 
-  def password_reset(email) do
+  def password_reset(user) do
+    base_email
+    |> to(user.email)
+    |> subject("Password reset")
+    |> assign(:user, user)
+    |> render("password_reset.html")
   end
 end
